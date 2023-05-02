70d040d689606d66570b0cbc6a0ec614d5419d42
cca <cca@DESKTOP-QLF1HVV>
2023-05-02 14:48:53 +0600

    Commited B+ Tree Assignment

---
+++ BPlusTreeDictionary/src/INode.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public interface INode<K extends Comparable<K>, V> {
+	void insert(K key, V value);
+
+	V get(K key);
+
+	void put(K key, V value);
+
+	List<K> getKeys();
+
+	int getOrder();
+
+	void remove(K key);
+
+	boolean isOverflow();
+
+	boolean isUnderflow();
+
+	List<INode<K, V>> split();
+
+	void addChild(INode<K, V> child);
+
+	List<INode<K, V>> getChildren();
+
+}
